3f1ac1cb7d95c7f3bc0bdbcffbce599aa076e750
Kelvin Creighton <kelvincreighton413@gmail.com>
2024-08-13 10:04:33 -0600

    Fixed blur swap - Finalized proof of concept
    This works at least for black and white which was what it was intended for. I will do more testing and clean up along with terminal ui in the future

--- PNGColourSwapper/pngcolourswap.py
+++ PNGColourSwapper/pngcolourswap.py
@@ -4,11 +4,15 @@ from PIL import Image
 def minmax_colour(num):
     return max(min(num, 255), 0)
 
+def swap_colour_with_blur(current_colour, from_colour, to_colour, buffer):
+    return ( to_colour[0] + buffer-(from_colour[0]-current_colour[0]), to_colour[1] + buffer-(from_colour[1]-current_colour[1]), to_colour[2] + buffer-(from_colour[2]-current_colour[2]) )
+
+
 def colour_in_buffer_range(colour, target_colour, buffer):
     if len(colour) != len(target_colour):
         return False
     for i in range(len(colour)):
-        if abs(colour[i] - target_colour[i]) > buffer:
+        if abs(target_colour[i] - colour[i]) > buffer:
             return False
     return True
 
@@ -25,10 +29,10 @@ def colour_swap(img_path, colour1, colour2, tolerance, blur_swap_tolerance):
         elif colour_in_buffer_range(item[:3], colour2[:3], tolerance):  # Swap colour 2 with colour 1
             new_data.append(colour1 + item[3:])
         elif colour_in_buffer_range(item[:3], colour1[:3], blur_swap_tolerance):
-            new_colour = (minmax_colour(colour2[0] + colour1[0] - item[0]), minmax_colour(colour2[1] + colour1[1] - item[1]), minmax_colour(colour2[2] + colour1[2] - item[2]))
+            new_colour = swap_colour_with_blur(item[:3], colour1[:3], colour2[:3], blur_swap_tolerance)
             new_data.append(new_colour + item[3:])
         elif colour_in_buffer_range(item[:3], colour2[:3], blur_swap_tolerance):
-            new_colour = (minmax_colour(colour1[0] + colour2[0] - item[0]), minmax_colour(colour1[1] + colour2[1] - item[1]), minmax_colour(colour1[2] + colour2[2] - item[2]))
+            new_colour = swap_colour_with_blur(item[:3], colour2[:3], colour1[:3], blur_swap_tolerance)
             new_data.append(new_colour + item[3:])
         else:
             new_data.append(item)
@@ -39,6 +43,6 @@ def colour_swap(img_path, colour1, colour2, tolerance, blur_swap_tolerance):
 input_img = 'Career_Tree_v1.png'
 colour1 = (0, 0, 0)
 colour2 = (255, 255, 255)
-tolerance = 0
-blur_swap_tolerance = 150
+tolerance = 255//4
+blur_swap_tolerance = 255//4
 colour_swap(input_img, colour1, colour2, tolerance, blur_swap_tolerance)
